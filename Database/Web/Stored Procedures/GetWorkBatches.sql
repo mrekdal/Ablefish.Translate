417c68bc3df5a7de8c95df538531e60450c92d78
Magne Rekdal <magne.rekdal@helse-bergen.no>
2026-02-12 08:20:43 +0100

    NEW: Routine to return the work batches retrieved by a certain user.

---
+++ Database/Web/Stored Procedures/GetWorkBatches.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE Web.GetWorkBatches( @LogTo VARCHAR(16) ) AS
+BEGIN
+  SELECT ubl.*, ul.FirstName 
+  FROM dbo.UserBatchLog ubl
+  JOIN dbo.UserList ul ON ul.LogTo = ubl.LogTo AND ( ul.LogTo = @LogTo OR ul.FirstName = @LogTo )
+  ORDER BY BatchId DESC;
+END
