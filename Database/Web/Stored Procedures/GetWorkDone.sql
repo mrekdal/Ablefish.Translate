1791a4ba02c2d1d49a5f0bb6d56b1228cfa466b5
Magne Rekdal <magne.rekdal@helse-bergen.no>
2026-02-12 08:09:13 +0100

    CHG: The GetWorkDone is reporting per project.

--- Database/Web/Stored Procedures/GetWorkDone.sql
+++ Database/Web/Stored Procedures/GetWorkDone.sql
@@ -1,9 +1,10 @@
 ﻿CREATE PROCEDURE [Web].[GetWorkDone]( @LogTo VARCHAR(16) = NULL ) AS 
 BEGIN
-  SELECT tb.LangKey, ul.FirstName, ul.LogTo, COUNT(*) AS n
+  SELECT wi.ProjectId, tb.LangKey, ul.FirstName, ul.LogTo, COUNT(*) AS n
   FROM dbo.TextBlock tb
   JOIN dbo.UserList ul ON ul.LogTo = tb.LogTo AND ul.MachineUser = 0
-  WHERE ISNULL(@LogTo,ul.LogTo) = ul.LogTo
-  GROUP BY ul.LogTo, ul.FirstName, tb.LangKey
-  ORDER by tb.LangKey, ul.FirstName
+  JOIN dbo.WorkItem wi ON wi.WorkId = tb.WorkId
+  WHERE ( ISNULL(@LogTo,ul.LogTo) = ul.LogTo ) OR ( ul.FirstName = @LogTo )
+  GROUP BY wi.ProjectId, ul.LogTo, ul.FirstName, tb.LangKey
+  ORDER by ul.FirstName, wi.ProjectId, tb.LangKey;
 END
