2c5b0f09650920797bb11b5e0d06906dfcc902b4
Magne Rekdal <magne.rekdal@helse-bergen.no>
2026-02-12 08:16:43 +0100

    NEW: Allowing users to be inactive.

--- Database/dbo/Tables/UserList.sql
+++ Database/dbo/Tables/UserList.sql
@@ -11,6 +11,7 @@
     [HelperLanguage] VARCHAR (12) DEFAULT ('nb') NOT NULL,
     [RowVer]         ROWVERSION   NOT NULL,
     [CreatedAt]      DATETIME     DEFAULT (getdate()) NOT NULL,
+    [IsActive]       BIT          DEFAULT ((1)) NOT NULL,
     CONSTRAINT [PK_UserList] PRIMARY KEY CLUSTERED ([LogTo] ASC),
     CONSTRAINT [FK_UserList_Helper] FOREIGN KEY ([HelperLanguage]) REFERENCES [dbo].[TextLanguage] ([LangKey]),
     CONSTRAINT [FK_UserList_ProjectId] FOREIGN KEY ([ProjectId]) REFERENCES [dbo].[Project] ([ProjectId]),
