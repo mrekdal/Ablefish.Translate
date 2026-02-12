5920d5fe0d492f0e8fdf3af80da6a689a7838e3d
Magne Rekdal <magne.rekdal@helse-bergen.no>
2026-02-12 08:19:49 +0100

    NEW: Routine to flag work items that the original needs fixing.

---
+++ Database/Web/Stored Procedures/FlagWorkItem.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE Web.FlagWorkItem( @WorkId INT, @FlaggedBy VARCHAR(16) ) AS 
+BEGIN
+  SET NOCOUNT ON;
+  UPDATE dbo.WorkItem SET Flagged = 1, FlaggedBy = @FlaggedBy 
+  WHERE WorkId = @WorkId;
+END
