edd4a8e4d4b69cc33799e4de4eda942e2ac3cd48
Magne Rekdal <magne.rekdal@helse-bergen.no>
2026-02-12 09:30:27 +0100

    CHG: Text block disagreements must match on CheckSrc.

--- Database/Web/Functions/GetTextBlockDisagreements.sql
+++ Database/Web/Functions/GetTextBlockDisagreements.sql
@@ -1,24 +1,11 @@
 ﻿CREATE FUNCTION [Web].[GetTextBlockDisagreements]( @LangKey VARCHAR(12) ) RETURNS TABLE AS
 RETURN
 (
-    SELECT *
-    FROM
-    (
-        SELECT 
-            tb.WorkId,
-            tb.LangKey,
-            MIN(tb.CheckRaw) AS MinCheck,
-            MAX(tb.CheckRaw) AS MaxCheck,
-            COUNT(*) AS n,
-            MIN(tb.LogTo) AS MinLogTo,
-            MAX(tb.LogTo) AS MaxLogTo
-        FROM dbo.TextBlock tb
-        INNER JOIN dbo.UserList ul 
-            ON ul.LogTo = tb.LogTo
-            AND ul.MachineUser = 0
-		WHERE tb.LangKey = @LangKey AND tb.IsDiscarded = 0
-        GROUP BY tb.WorkId, tb.LangKey
-        HAVING COUNT(*) > 1
-    ) agg
-    WHERE agg.MinCheck <> agg.MaxCheck
+    SELECT tb.WorkId, @LangKey AS LangKey, MIN(tb.CheckRaw) AS MinCheck, MAX(tb.CheckRaw) AS MaxCheck, MIN(tb.LogTo) AS MinLogTo, MAX(tb.LogTo) AS MaxLogTo, COUNT(*) AS n
+    FROM dbo.TextBlock tb
+	JOIN dbo.WorkItem wi ON wi.WorkId = tb.WorkId AND wi.CheckSrc = tb.CheckSrc -- Limit to active originals
+    JOIN dbo.UserList ul ON ul.LogTo = tb.LogTo AND ul.MachineUser = 0 -- Limit to non-machine users.
+    WHERE tb.LangKey = @LangKey AND tb.IsDiscarded = 0 -- Exclude discarded text blocks
+    GROUP BY tb.WorkId
+    HAVING COUNT(*) > 1 AND MIN(tb.CheckRaw) <> MAX(tb.CheckRaw)
 );
