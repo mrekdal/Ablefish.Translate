14195c0fa2eb8ef50afbc8cc394c045f00becd01
Magne Rekdal <magne.rekdal@helse-bergen.no>
2026-02-12 08:10:43 +0100

    BUGFIX: Including CheckSrc on the project status report, to handle when the original WorkItem.

--- Database/Web/Stored Procedures/GetProjectStatus.sql
+++ Database/Web/Stored Procedures/GetProjectStatus.sql
@@ -1,4 +1,4 @@
-﻿CREATE PROCEDURE Web.GetProjectStatus( @LogTo VARCHAR(16) ) AS
+﻿CREATE PROCEDURE [Web].[GetProjectStatus]( @LogTo VARCHAR(16) ) AS
 BEGIN
   SELECT p.ProjectId, p.ProjectName, p.ShortName, utl.LangKey,
     COUNT(*) AS WorkTotal, 
@@ -7,6 +7,6 @@ BEGIN
   JOIN dbo.WorkItem wi ON wi.ProjectId = p.ProjectId
   JOIN dbo.UserProject up ON up.ProjectId = p.ProjectId AND up.LogTo = @LogTo
   JOIN dbo.UserTargetLanguage utl ON utl.LogTo = up.LogTo AND utl.IsActive = 1
-  LEFT JOIN dbo.TextBlock tb ON tb.WorkId = wi.WorkId AND tb.LogTo = up.LogTo AND tb.LangKey = utl.LangKey
+  LEFT JOIN dbo.TextBlock tb ON tb.WorkId = wi.WorkId AND tb.CheckSrc = wi.CheckSrc AND tb.LogTo = up.LogTo AND tb.LangKey = utl.LangKey
   GROUP BY p.ProjectId, p.ProjectName, p.ShortName, utl.LangKey;
 END
