b33f234b4867b00253550730c44a1c73ed2af286
Magne Rekdal <magne.rekdal@helse-bergen.no>
2026-02-12 08:33:53 +0100

    NEW: Routine to return all the the approved translations, used to post them back to the original database.

---
+++ Database/Web/Functions/GetApprovedTranslations.sql
@@ -0,0 +1,12 @@
+﻿CREATE FUNCTION Web.GetApprovedTranslations( @ProjectId INT, @LangTrg VARCHAR(12) ) RETURNS TABLE AS RETURN
+(
+  SELECT wi.WorkId, wi.RowKey, tb.RawText, wi.CheckSrc, tb.CheckRaw,
+         COUNT(*) AS Cnt, MIN(tb.LogTo) AS FirstLogTo, MAX(tb.LogTo) AS LastLogTo
+  FROM dbo.WorkItem wi
+  JOIN dbo.TextApproved ta
+    ON ta.WorkId = wi.WorkId AND ta.CheckSrc = wi.CheckSrc
+  JOIN dbo.TextBlock tb
+    ON tb.WorkId = wi.WorkId AND tb.CheckRaw = ta.CheckTrg
+  WHERE wi.ProjectId = @ProjectId AND ta.LangTrg = @LangTrg
+  GROUP BY wi.WorkId, wi.RowKey, tb.RawText, wi.CheckSrc, tb.CheckRaw
+);
