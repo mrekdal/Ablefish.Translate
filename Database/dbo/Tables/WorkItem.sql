c0cc0c8ad27849462b12cbb9d99eeee13e2e4626
Magne Rekdal <magne.rekdal@helse-bergen.no>
2026-02-12 08:23:18 +0100

    NEW: Added support for flagging of work Items. NEW: Added support for system-versioning of work items.

--- Database/dbo/Tables/WorkItem.sql
+++ Database/dbo/Tables/WorkItem.sql
@@ -1,15 +1,24 @@
 ﻿CREATE TABLE [dbo].[WorkItem] (
-    [WorkId]    INT            IDENTITY (1, 1) NOT NULL,
-    [ProjectId] INT            NOT NULL,
-    [RowKey]    VARCHAR (64)   NOT NULL,
-    [RawText]   NVARCHAR (MAX) NOT NULL,
-    [CheckSrc]  AS             (checksum([RawText])),
-    [RowVer]    ROWVERSION     NOT NULL,
-    [CreatedAt] DATETIME       DEFAULT (getdate()) NOT NULL,
-    [UpdatedAt] DATETIME       DEFAULT (getdate()) NOT NULL,
+    [WorkId]    INT                                         IDENTITY (1, 1) NOT NULL,
+    [ProjectId] INT                                         NOT NULL,
+    [RowKey]    VARCHAR (64)                                NOT NULL,
+    [RawText]   NVARCHAR (MAX)                              NOT NULL,
+    [CheckSrc]  AS                                          (checksum([RawText])),
+    [RowVer]    ROWVERSION                                  NOT NULL,
+    [CreatedAt] DATETIME                                    DEFAULT (getdate()) NOT NULL,
+    [UpdatedAt] DATETIME                                    DEFAULT (getdate()) NOT NULL,
+    [Flagged]   BIT                                         DEFAULT ((0)) NOT NULL,
+    [FlaggedBy] VARCHAR (16)                                NULL,
+    [ValidFrom] DATETIME2 (7) GENERATED ALWAYS AS ROW START DEFAULT (sysutcdatetime()) NOT NULL,
+    [ValidTo]   DATETIME2 (7) GENERATED ALWAYS AS ROW END   DEFAULT (CONVERT([datetime2](7),'9999-12-31 23:59:59.9999999')) NOT NULL,
     CONSTRAINT [PK_WorkItem] PRIMARY KEY CLUSTERED ([WorkId] ASC),
-    CONSTRAINT [FK_WorkItem_ProjectId] FOREIGN KEY ([ProjectId]) REFERENCES [dbo].[Project] ([ProjectId])
-);
+    FOREIGN KEY ([FlaggedBy]) REFERENCES [dbo].[UserList] ([LogTo]),
+    CONSTRAINT [FK_WorkItem_ProjectId] FOREIGN KEY ([ProjectId]) REFERENCES [dbo].[Project] ([ProjectId]),
+    PERIOD FOR SYSTEM_TIME ([ValidFrom], [ValidTo])
+)
+WITH (SYSTEM_VERSIONING = ON (HISTORY_TABLE=[dbo].[MSSQL_TemporalHistoryFor_338100245], DATA_CONSISTENCY_CHECK=ON));
+
+
 
 
 
@@ -26,4 +35,10 @@ CREATE UNIQUE NONCLUSTERED INDEX [UIDX_WorkItem_ProjectId_RowKey]
 GO
 CREATE NONCLUSTERED INDEX [IDX_WorkItem_CheckSrc]
     ON [dbo].[WorkItem]([CheckSrc] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IDX_WorkItem_ProjectId]
+    ON [dbo].[WorkItem]([ProjectId] ASC)
+    INCLUDE([CheckSrc]);
 
