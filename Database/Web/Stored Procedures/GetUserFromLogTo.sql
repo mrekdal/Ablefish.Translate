fadfc1f6d02800cd88dae26403ef97d3f1f5c259
Magne Rekdal <magne.rekdal@helse-bergen.no>
2026-02-12 08:11:23 +0100

    CHG: Errors raised when LogTo is not a valid user.

--- Database/Web/Stored Procedures/GetUserFromLogTo.sql
+++ Database/Web/Stored Procedures/GetUserFromLogTo.sql
@@ -1,6 +1,15 @@
 ﻿CREATE PROCEDURE [Web].[GetUserFromLogTo] ( @LogTo VARCHAR(16) ) AS
 BEGIN
   SET NOCOUNT ON;
-  SELECT* FROM dbo.UserList WHERE LogTo = @LogTo;
-  INSERT INTO dbo.UserLog( LogTo ) VALUES( @LogTo );
+  DECLARE @IsActive BIT;
+  SELECT @IsActive = IsActive FROM dbo.UserList WHERE LogTo = @LogTo;
+  IF @IsActive IS NULL
+    RAISERROR ( 'There is no such user.', 16, 1 );
+  ELSE IF @IsActive = 0
+    RAISERROR ( 'This user has been deactivated.', 16, 1 );
+  ELSE 
+  BEGIN
+    INSERT INTO dbo.UserLog( LogTo ) VALUES( @LogTo );
+    SELECT * FROM dbo.UserList WHERE LogTo = @LogTo;
+  END;
 END
