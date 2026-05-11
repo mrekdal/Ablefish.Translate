0f487418abc6bf15c788a8eb9210ae2e8397a500
Magne Rekdal <magne.rekdal@helse-bergen.no>
2026-05-11 09:42:38 +0200

    NEW: Versioning of TextBlock.

--- Database/dbo/Tables/TextBlock.sql
+++ Database/dbo/Tables/TextBlock.sql
@@ -1,22 +1,28 @@
 ﻿CREATE TABLE [dbo].[TextBlock] (
-    [BlockId]     INT            IDENTITY (1, 1) NOT NULL,
-    [LangKey]     VARCHAR (12)   NULL,
-    [RawText]     NVARCHAR (MAX) NULL,
-    [WorkId]      INT            NULL,
-    [LogTo]       VARCHAR (16)   DEFAULT ('public') NOT NULL,
-    [RowVer]      ROWVERSION     NOT NULL,
-    [CreatedAt]   DATETIME       DEFAULT (getdate()) NOT NULL,
-    [CheckRaw]    AS             (checksum([RawText])) PERSISTED,
-    [IsDiscarded] BIT            DEFAULT ((0)) NOT NULL,
-    [DiscardedBy] VARCHAR (16)   NULL,
-    [WithDoubt]   BIT            DEFAULT ((0)) NOT NULL,
-    [CheckSrc]    INT            NOT NULL,
+    [BlockId]     INT                                         IDENTITY (1, 1) NOT NULL,
+    [LangKey]     VARCHAR (12)                                NULL,
+    [RawText]     NVARCHAR (MAX)                              NULL,
+    [WorkId]      INT                                         NULL,
+    [LogTo]       VARCHAR (16)                                DEFAULT ('public') NOT NULL,
+    [RowVer]      ROWVERSION                                  NOT NULL,
+    [CreatedAt]   DATETIME                                    DEFAULT (getdate()) NOT NULL,
+    [CheckRaw]    AS                                          (checksum([RawText])) PERSISTED,
+    [IsDiscarded] BIT                                         DEFAULT ((0)) NOT NULL,
+    [DiscardedBy] VARCHAR (16)                                NULL,
+    [WithDoubt]   BIT                                         DEFAULT ((0)) NOT NULL,
+    [CheckSrc]    INT                                         NOT NULL,
+    [ValidFrom]   DATETIME2 (7) GENERATED ALWAYS AS ROW START CONSTRAINT [DF_TextBlock_ValidFrom] DEFAULT (sysutcdatetime()) NOT NULL,
+    [ValidTo]     DATETIME2 (7) GENERATED ALWAYS AS ROW END   CONSTRAINT [DF_TextBlock_ValidTo] DEFAULT (CONVERT([datetime2](7),'9999-12-31 23:59:59.9999999')) NOT NULL,
     CONSTRAINT [PK_BlockId] PRIMARY KEY CLUSTERED ([BlockId] ASC),
     CONSTRAINT [FK_TextBlock_DiscardedBy] FOREIGN KEY ([DiscardedBy]) REFERENCES [dbo].[UserList] ([LogTo]),
     CONSTRAINT [FK_TextBlock_LangKey] FOREIGN KEY ([LangKey]) REFERENCES [dbo].[TextLanguage] ([LangKey]),
     CONSTRAINT [FK_TextBlock_LogTo] FOREIGN KEY ([LogTo]) REFERENCES [dbo].[UserList] ([LogTo]) ON UPDATE CASCADE,
-    CONSTRAINT [FK_TextBlock_WorkId] FOREIGN KEY ([WorkId]) REFERENCES [dbo].[WorkItem] ([WorkId])
-);
+    CONSTRAINT [FK_TextBlock_WorkId] FOREIGN KEY ([WorkId]) REFERENCES [dbo].[WorkItem] ([WorkId]),
+    PERIOD FOR SYSTEM_TIME ([ValidFrom], [ValidTo])
+)
+WITH (SYSTEM_VERSIONING = ON (HISTORY_TABLE=[dbo].[TextBlockHistory], DATA_CONSISTENCY_CHECK=ON));
+
+
 
 
 
---
+++ Database/dbo/Tables/TextBlockHistory.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].[TextBlockHistory] (
+    [BlockId]     INT            NOT NULL,
+    [LangKey]     VARCHAR (12)   NULL,
+    [RawText]     NVARCHAR (MAX) NULL,
+    [WorkId]      INT            NULL,
+    [LogTo]       VARCHAR (16)   NOT NULL,
+    [RowVer]      ROWVERSION     NOT NULL,
+    [CreatedAt]   DATETIME       NOT NULL,
+    [CheckRaw]    INT            NULL,
+    [IsDiscarded] BIT            NOT NULL,
+    [DiscardedBy] VARCHAR (16)   NULL,
+    [WithDoubt]   BIT            NOT NULL,
+    [CheckSrc]    INT            NOT NULL,
+    [ValidFrom]   DATETIME2 (7)  NOT NULL,
+    [ValidTo]     DATETIME2 (7)  NOT NULL
+);
+
+
+GO
+CREATE CLUSTERED INDEX [ix_TextBlockHistory]
+    ON [dbo].[TextBlockHistory]([ValidTo] ASC, [ValidFrom] ASC) WITH (DATA_COMPRESSION = PAGE);
+
