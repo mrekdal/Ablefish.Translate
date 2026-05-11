61894a4bb645641643400cb581db755311c22a83
Magne Rekdal <magne.rekdal@helse-bergen.no>
2026-05-11 09:41:10 +0200

    BUGFIX: Adding source checksum to the TextBlock translation. CAVEAT: Requires import to be done quickly, after source is changed.

--- Database/dbo/Stored Procedures/AddResourceText.sql
+++ Database/dbo/Stored Procedures/AddResourceText.sql
@@ -2,11 +2,11 @@
 BEGIN
   SET NOCOUNT ON;
   DECLARE @WorkId INT;
-  DECLARE @CheckSum INT;
+  DECLARE @CheckSrc INT;
   IF @LangKey = 'en-GB' 
     EXEC dbo.AddWorkItem @ProjectId, @RowKey, @RawText;
   ELSE BEGIN
-    SELECT @WorkId = WorkId FROM dbo.WorkItem WHERE ProjectId = @ProjectId AND RowKey = @RowKey;
-	EXEC dbo.AddTextBlockRowKey @ProjectId, @RowKey, @LangKey, @RawText, 'ResX';
+    SELECT @WorkId = WorkId, @CheckSrc = CheckSrc FROM dbo.WorkItem WHERE ProjectId = @ProjectId AND RowKey = @RowKey;
+	EXEC dbo.AddTextBlockRowKey @ProjectId, @RowKey, @LangKey, @RawText, @CheckSrc, 'ResX';
   END;
 END
