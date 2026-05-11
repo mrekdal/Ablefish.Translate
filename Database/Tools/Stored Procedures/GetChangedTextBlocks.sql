23364cc5aea5298052e098ea69bf0e3de1e5fde2
Magne Rekdal <magne.rekdal@helse-bergen.no>
2026-05-11 09:44:40 +0200

    NEW: Procedures for changed work items and text blocks.

---
+++ Database/Tools/Stored Procedures/GetChangedTextBlocks.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE [Tools].[GetChangedTextBlocks]( @ProjectId INT, @StartAt DATETIME ) AS
+BEGIN
+  WITH Versions AS
+  (
+    SELECT
+        tb.BlockId,
+        tb.RawText,
+        tb.ValidFrom,
+        tb.ValidTo,
+        LAG(tb.RawText) OVER (
+            PARTITION BY tb.BlockId
+            ORDER BY tb.ValidFrom
+        ) AS OldRawText
+    FROM dbo.TextBlock FOR SYSTEM_TIME ALL AS tb
+	JOIN dbo.WorkItem wi ON wi.WorkId = tb.WorkId
+	WHERE wi.ProjectId = @ProjectId
+  )
+  SELECT
+    BlockId,
+    OldRawText,
+    RawText AS NewRawText,
+    ValidFrom AS ChangedAt
+  FROM Versions
+  WHERE OldRawText IS NOT NULL
+    AND OldRawText <> RawText
+    AND ValidFrom >= @StartAt
+  ORDER BY ChangedAt DESC;
+END
---
+++ Database/Tools/Stored Procedures/GetChangedWorkItems.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE [Tools].[GetChangedWorkItems]( @ProjectId INT, @StartAt DATETIME ) AS
+BEGIN
+  WITH Versions AS
+  (
+    SELECT
+        WorkId,
+        RawText,
+        ValidFrom,
+        ValidTo,
+        LAG(RawText) OVER (
+            PARTITION BY WorkId
+            ORDER BY ValidFrom
+        ) AS OldRawText
+    FROM dbo.WorkItem
+	FOR SYSTEM_TIME ALL
+	WHERE ProjectId = @ProjectId
+  )
+  SELECT
+    WorkId,
+    OldRawText,
+    RawText AS NewRawText,
+    ValidFrom AS ChangedAt
+  FROM Versions
+  WHERE OldRawText IS NOT NULL
+    AND OldRawText <> RawText
+    AND ValidFrom >= @StartAt
+  ORDER BY ChangedAt DESC;
+END
