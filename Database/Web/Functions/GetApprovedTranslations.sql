ca4f17a90ab34e0159f7c6bd8f123db0f2bdbec9
Magne Rekdal <magne.rekdal@helse-bergen.no>
2026-05-11 09:44:10 +0200

    NEW: Only allowing non-machine users to approve text, and making sure that source hasn't changed.

--- Database/Web/Functions/GetApprovedTranslations.sql
+++ Database/Web/Functions/GetApprovedTranslations.sql
@@ -1,4 +1,4 @@
-﻿CREATE FUNCTION Web.GetApprovedTranslations( @ProjectId INT, @LangTrg VARCHAR(12) ) RETURNS TABLE AS RETURN
+﻿CREATE FUNCTION [Web].[GetApprovedTranslations]( @ProjectId INT, @LangTrg VARCHAR(12) ) RETURNS TABLE AS RETURN
 (
   SELECT wi.WorkId, wi.RowKey, tb.RawText, wi.CheckSrc, tb.CheckRaw,
          COUNT(*) AS Cnt, MIN(tb.LogTo) AS FirstLogTo, MAX(tb.LogTo) AS LastLogTo
@@ -6,7 +6,8 @@
   JOIN dbo.TextApproved ta
     ON ta.WorkId = wi.WorkId AND ta.CheckSrc = wi.CheckSrc
   JOIN dbo.TextBlock tb
-    ON tb.WorkId = wi.WorkId AND tb.CheckRaw = ta.CheckTrg
+    ON tb.WorkId = wi.WorkId AND tb.CheckSrc = ta.CheckSrc AND tb.CheckRaw = ta.CheckTrg
+  JOIN dbo.UserList ul ON ul.LogTo = tb.LogTo AND ul.MachineUser = 0
   WHERE wi.ProjectId = @ProjectId AND ta.LangTrg = @LangTrg
   GROUP BY wi.WorkId, wi.RowKey, tb.RawText, wi.CheckSrc, tb.CheckRaw
 );
