5aad43cfe7a03014e3be387b7d0deffbc46cc4a2
Magne Rekdal <magne.rekdal@helse-bergen.no>
2026-05-11 09:41:45 +0200

    BUGFIX: Adding source checksum to the TextBlock translation. CAVEAT: Requires import to be done quickly, after source is changed.

--- Database/dbo/Stored Procedures/AddTextBlockRowKey.sql
+++ Database/dbo/Stored Procedures/AddTextBlockRowKey.sql
@@ -1,11 +1,11 @@
-﻿CREATE PROCEDURE [dbo].[AddTextBlockRowKey]( @ProjectId INT, @RowKey VARCHAR(64), @LangKey VARCHAR(12), @RawText NVARCHAR(MAX), @LogTo VARCHAR(16) ) AS
+﻿CREATE PROCEDURE [dbo].[AddTextBlockRowKey]( @ProjectId INT, @RowKey VARCHAR(64), @LangKey VARCHAR(12), @RawText NVARCHAR(MAX), @CheckSrc INT, @LogTo VARCHAR(16) ) AS
 BEGIN
 
   SET NOCOUNT ON;
 
   -- Find all relevant WorkIds.
 
-  SELECT w2.WorkId 
+  SELECT w2.WorkId  
   INTO #t
   FROM dbo.WorkItem wi
   JOIN dbo.WorkItem w2 ON w2.ProjectId = wi.ProjectId AND w2.RawText = wi.RawText AND w2.CheckSrc = wi.CheckSrc
@@ -17,8 +17,8 @@ BEGIN
   USING ( SELECT WorkId FROM #t ) AS src
   ON src.WorkId = trg.WorkId AND trg.LangKey = @LangKey AND trg.LogTo = @LogTo
   WHEN MATCHED THEN
-    UPDATE SET trg.RawText = @RawText
+    UPDATE SET trg.RawText = @RawText, CheckSrc = @CheckSrc
   WHEN NOT MATCHED THEN
-    INSERT ( WorkId, LangKey, RawText, LogTo ) VALUES ( src.WorkId, @LangKey, @RawText, @LogTo );
+    INSERT ( WorkId, LangKey, RawText, CheckSrc, LogTo ) VALUES ( src.WorkId, @LangKey, @RawText, @CheckSrc, @LogTo );
 
 END
